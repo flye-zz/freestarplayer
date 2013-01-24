fbac372ee34413d581f56f37c592c19612ca61e9
yefl <yefl@lemote.com>
2013-01-24 15:21:06 +0800

    从demo复制出代码，开始加自己的功能并测试

--- FreeStarPlayer/src/com/freestar/player/MainActivity.java
+++ FreeStarPlayer/src/com/freestar/player/MainActivity.java
@@ -1,24 +1,60 @@
 package com.freestar.player;
 
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
+/*
+ * Copyright (C) 2012 flye
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author flye E-mail:flyearlyguys.com
+ * @version 创建时间：2013-1-24 下午3:18:00
+ * 类说明
+ */
+
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private String path = "/sdcard/sintel.mp4";
+	private VideoView mVideoView;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		if (!io.vov.vitamio.LibsChecker.checkVitamioLibs(this))
+			return;
 
+		setContentView(R.layout.activity_main);
+		mVideoView = (VideoView) findViewById(R.id.surface_view);
+		mVideoView.setVideoPath(path);
+		mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);
+		mVideoView.setMediaController(new MediaController(this));
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		if (mVideoView != null)
+			mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
+		super.onConfigurationChanged(newConfig);
+	}
     
 }
