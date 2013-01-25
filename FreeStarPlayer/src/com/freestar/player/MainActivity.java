b2df678769bdd7b1ea667e4890c947f9052f6e94
yefl <yefl@lemote.com>
2013-01-25 10:36:13 +0800

    增加手势控制音量； 增加手势控制亮度 ps:在M1手机测试正常，在华为mediapad-10测试时当用物理按键把音量调节到最低静音时，手势调节无法打开声音。why？？

--- FreeStarPlayer/src/com/freestar/player/MainActivity.java
+++ FreeStarPlayer/src/com/freestar/player/MainActivity.java
@@ -3,11 +3,21 @@ package com.freestar.player;
 import io.vov.vitamio.MediaPlayer;
 import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.util.Log;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.WindowManager;
 import android.support.v4.app.NavUtils;
 
 /*
@@ -36,6 +46,14 @@ public class MainActivity extends Activity {
 
 	private String path = "/sdcard/sintel.mp4";
 	private VideoView mVideoView;
+	private GestureDetector mGestureDetector;
+	private AudioManager mAudioManager;
+	/** 最大声音 */
+    private int mMaxVolume;
+    /** 当前声音 */
+    private int mVolume = -1;
+    /** 当前亮度 */
+    private float mBrightness = -1f;
 
 	@Override
 	public void onCreate(Bundle icicle) {
@@ -44,10 +62,19 @@ public class MainActivity extends Activity {
 			return;
 
 		setContentView(R.layout.activity_main);
+		mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mMaxVolume = mAudioManager
+                 .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		
+		
 		mVideoView = (VideoView) findViewById(R.id.surface_view);
 		mVideoView.setVideoPath(path);
 		mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);
 		mVideoView.setMediaController(new MediaController(this));
+		
+		mGestureDetector = new GestureDetector(this, new MyGestureListener());
+		
+		
 	}
 
 	@Override
@@ -56,5 +83,106 @@ public class MainActivity extends Activity {
 			mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
 		super.onConfigurationChanged(newConfig);
 	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector.onTouchEvent(event))
+            return true;
+
+        // 处理手势结束
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        case MotionEvent.ACTION_UP:
+            endGesture();
+            break;
+        }
+
+        return super.onTouchEvent(event);
+	}
+	
+	   /** 手势结束 */
+    private void endGesture() {
+        mVolume = -1;
+        mBrightness = -1f;
+
+        // 隐藏
+        mDismissHandler.removeMessages(0);
+        mDismissHandler.sendEmptyMessageDelayed(0, 500);
+    } 
     
+    /** 定时隐藏 */
+    private Handler mDismissHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+//            mVolumeBrightnessLayout.setVisibility(View.GONE);
+        }
+    };
+	private class MyGestureListener extends SimpleOnGestureListener{
+
+		@Override
+		public boolean onDoubleTap(MotionEvent e) {
+			// TODO Auto-generated method stub
+			return super.onDoubleTap(e);
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			float mOldX = e1.getX(), mOldY = e1.getY();
+            int y = (int) e2.getRawY();
+            Display disp = getWindowManager().getDefaultDisplay();
+            int windowWidth = disp.getWidth();
+            int windowHeight = disp.getHeight();
+
+            if (mOldX > windowWidth * 4.0 / 5)// 右边滑动
+                onVolumeSlide((mOldY - y) / windowHeight);
+            else if (mOldX < windowWidth / 5.0)// 左边滑动
+                onBrightnessSlide((mOldY - y) / windowHeight);
+            return super.onScroll(e1, e2, distanceX, distanceY);
+		}
+	}
+	
+	/**
+	 *滑动改变声音
+	 */
+	private void onVolumeSlide(float percent) {
+        if (mVolume == -1) {
+            mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            if (mVolume < 0)
+                mVolume = 0;
+        }
+
+        int index = (int) (percent * mMaxVolume) + mVolume;
+        if (index > mMaxVolume)
+            index = mMaxVolume;
+        else if (index < 0){
+            index = 0;
+            mAudioManager.setVibrateSetting(AudioManager.VIBRATE_TYPE_NOTIFICATION , AudioManager.VIBRATE_SETTING_OFF);
+        }
+        // 变更声音
+        mAudioManager.setStreamMute(AudioManager.STREAM_MUSIC, false);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, index, 1);
+        Log.d("test", String.valueOf(mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC)));
+    }
+	
+    /**
+     * 滑动改变亮度
+     */
+    private void onBrightnessSlide(float percent) {
+        if (mBrightness < 0) {
+            mBrightness = getWindow().getAttributes().screenBrightness;
+            if (mBrightness <= 0.00f)
+                mBrightness = 0.50f;
+            if (mBrightness < 0.01f)
+                mBrightness = 0.01f;
+        }
+        WindowManager.LayoutParams lpa = getWindow().getAttributes();
+        lpa.screenBrightness = mBrightness + percent;
+        if (lpa.screenBrightness > 1.0f)
+            lpa.screenBrightness = 1.0f;
+        else if (lpa.screenBrightness < 0.01f)
+            lpa.screenBrightness = 0.01f;
+        getWindow().setAttributes(lpa);
+
+    }
+	
 }
